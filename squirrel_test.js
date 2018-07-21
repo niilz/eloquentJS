20a28de4b5fa3fc7de144206cd78b6056a20833a
nilSinger <niilz@outlook.de>
2018-07-21 20:57:47 +0200

    finshed squirrel chapter

--- squirrel_test.js
+++ squirrel_test.js
@@ -46,5 +46,16 @@ function journalEvents(journal) {
 //console.log(journalEvents(JOURNAL));
 
 for (let event of journalEvents(JOURNAL)) {
-    console.log(event + ":", phi(tableFor(event, JOURNAL)));
+    let correlation = phi(tableFor(event, JOURNAL))
+    if (correlation > 0.1 || correlation < -0.1) {
+        console.log(event + ":", correlation);
+    }
 }
+
+for (let entry of JOURNAL) {
+    if (entry.events.includes("peanuts") &&
+       !entry.events.includes("brushed teeth")) {
+       entry.events.push("peanut teeth");
+    }
+}
+console.log(phi(tableFor("peanut teeth", JOURNAL)));
